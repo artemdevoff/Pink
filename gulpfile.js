49484f1c4990ad68a6426c8be61deda834926481
frontsilent <silent0172@gmail.com>
2018-06-05 11:31:38 +0300

    completed final version

--- gulpfile.js
+++ gulpfile.js
@@ -99,8 +99,6 @@ gulp.task("clean", function () {
 
 gulp.task("build", function (fn) {
   run(
-//    "clean",
-//    "copy",
     "style",
     fn
   );
--- js/svgsprite.js
+++ js/svgsprite.js
@@ -25,3 +25,17 @@ ajax.onload = function(e) {
 		}, e.send()
 	} catch (j) {}
 }(window, document);
+
+var img = new Array();
+img[0] = new Image();
+img[1] = new Image();
+img[2] = new Image();
+img[3] = new Image();
+img[4] = new Image();
+
+
+img[0].src = "img/logo-mob-hover.svg"
+img[1].src = "img/logo-tablet-hover.svg"
+img[2].src = "img/logo-desc-hover.svg"
+img[3].src = "img/icons/open-btn-hover.svg"
+img[4].src = "img/icons/close-btn-hover.svg"
